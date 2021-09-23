0ab27ec42fa5a3f977c5b67c4d96b479e9919b57
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-09-23 20:27:42 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -129,7 +129,7 @@ async def auto_filter(bot, update):
     else:
         Send_message = await bot.send_message(
             chat_id=update.chat.id,
-            text="<b>Couldn't Find This Movie.Try Again ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>",
+            text="<b>PLZ WAIT BRO 😊</b>",
             reply_to_message_id=update.message_id
         )
         await asyncio.sleep(5)
