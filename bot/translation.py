22c3692da1e2b86e655c31ba784af93b7f4efd1a
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-09-27 00:22:26 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,55 +4,16 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+    START_TEXT = """ <b>Hi {} 😌
+I'm officially made for ~ @movies_Hubh2...😉\n
+You can Request Any Movie you want at our Group\n& I will provide the Files🗂️ in the Form of ~\n 🔗 Button Filters.\n
+For More Details Click /help Button ..\n
+             ~🇲‌🇦‌🇳‌🇦‌🇬‌🇪‌🇷‌ᵒᶠ movieshub
+</b>"""   
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+<b><u>ഞാൻ ഒരു പാട്ട് പറയാം അത് ഏത് സിനിമയിലെ ആണെന്ന് പറഞ്ഞാൽ ഹെല്പ് ചെയ്യാം</b></u>😎🎶🎶 <b><i> "എന്തരോ മഹാനു ഭാവുലൂ"</b></i> <b><u>അറിയാമെങ്കിൽ /about 👈ഇതിൽ ക്ലിക്ക് ചെയ്യ്</b></u>"""
     
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
-"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+    ABOUT_TEXT = """<b>എടാ ബുദ്ധിമാനെ ഐഡിയ കൊള്ളാം ബട്ട്‌ ഏറ്റില്ല🤭🤣 ഹെല്പ് അതും നിനക്ക് ഓട്രാ 🏃</b>
+<b>➥നിന്റെ ഫുൾ Details കാണണോ👉Click Deatils Mwonoo😎</b> : <i><a href="https://t.me/bgeejji/52">👉DETAILS👈</a></i>
 """
